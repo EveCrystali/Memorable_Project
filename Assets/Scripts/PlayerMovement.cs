63af55d68ee3a3f3dd19f9d7cf5b44b998ac1811
Pierobigno <116516473+Pierobigno@users.noreply.github.com>
2024-02-08 20:54:43 +0100

    08-02-2024
    Téléchargement des packages:
    - HDRP
    - Materials
    - Prefabs
    
    Création des 2 caméras avec layers différentes pour les 2 visions de joueurs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform playerTarget;
-    public float moveSpeed;
-    public Vector3 direction;
-    public Vector3 orientation;
     private PlayerInput input;
     private Gamepad gamepad;
-    public float turnSpeed;
+    public float moveSpeed;
+    public float rotationSpeed;
+    public Transform playerTarget;
+
+    private Quaternion targetRotation;
 
     void Awake()
     {
@@ -26,7 +25,8 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, playerTarget.position, moveSpeed * Time.deltaTime);
+        Vector2 direction = Vector2.zero;
+        Vector2 orientation = Vector2.zero;
 
         if (gamepad != null)
         {
@@ -35,19 +35,19 @@ public class PlayerMovement : MonoBehaviour
         }
 
         MovePlayerTarget(direction);
-        MovePlayerVision(orientation);
+        RotatePlayerVision(orientation);
     }
 
-    void MovePlayerTarget(Vector3 direction)
+    void MovePlayerTarget(Vector2 direction)
     {
-        direction.Normalize(); // Évite les déplacements diagonaux plus rapides
-        Vector3 moveDirection = new Vector3(direction.x, 0f, direction.y); // Utilise la composante Z à la place de Y
+        direction.Normalize();
+        Vector3 moveDirection = new Vector3(direction.x, 0f, direction.y);
         playerTarget.Translate(moveDirection * moveSpeed * Time.deltaTime);
     }
 
-    void MovePlayerVision(Vector3 orientation)
+    void RotatePlayerVision(Vector2 orientation)
     {
-        
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(orientation.x, 0f, orientation.y));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
-
 }
